393823359650be2dd896b179a24f9dfdb3b849fe
Sergei <sk_developer@mail.ru>
2023-09-23 14:50:28 +0300

    Разбил приложение на слои

--- app/src/main/java/com/synergysport/synergysportandroid/presentation/MainActivity.kt
+++ app/src/main/java/com/synergysport/synergysportandroid/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.synergysport.synergysportandroid
+package com.synergysport.synergysportandroid.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.synergysport.synergysportandroid.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
