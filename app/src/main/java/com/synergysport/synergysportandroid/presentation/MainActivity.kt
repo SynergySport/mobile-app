038b3c3e261288d3bb1973647e331347daad7eb2
Sergei <sk_developer@mail.ru>
2023-09-23 15:21:45 +0300

    Добавлен экран авторизации

--- app/src/main/java/com/synergysport/synergysportandroid/presentation/MainActivity.kt
+++ app/src/main/java/com/synergysport/synergysportandroid/presentation/MainActivity.kt
@@ -3,16 +3,25 @@ package com.synergysport.synergysportandroid.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.synergysport.synergysportandroid.R
+import com.synergysport.synergysportandroid.presentation.auth.AuthScreenFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setMainScreen()
+        setAuthScreen()
     }
 
     private fun setMainScreen() {
         val fragment = MainScreenFragment()
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.fragment_container, fragment)
+            .commit()
+    }
+
+    private fun setAuthScreen() {
+        val fragment = AuthScreenFragment()
         supportFragmentManager
             .beginTransaction()
             .add(R.id.fragment_container, fragment)
---
+++ app/src/main/java/com/synergysport/synergysportandroid/presentation/auth/AuthScreenFragment.kt
@@ -0,0 +1,7 @@
+package com.synergysport.synergysportandroid.presentation.auth
+
+import androidx.fragment.app.Fragment
+import com.synergysport.synergysportandroid.R
+
+class AuthScreenFragment : Fragment(R.layout.fragment_auth_screen) {
+}
