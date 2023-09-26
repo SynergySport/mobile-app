0cbb02dd51f8b6b1afe98017598f78d8b83bae9f
Sergei <sk_developer@mail.ru>
2023-09-26 15:30:36 +0300

    Сделан переход со стартового экрана на экран авторизации

--- app/src/main/java/com/synergysport/synergysportandroid/presentation/firstStartFragment/FirstStartFragment.kt
+++ app/src/main/java/com/synergysport/synergysportandroid/presentation/firstStartFragment/FirstStartFragment.kt
@@ -1,8 +1,26 @@
 package com.synergysport.synergysportandroid.presentation.firstStartFragment
 
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.synergysport.synergysportandroid.R
+import com.synergysport.synergysportandroid.presentation.auth.AuthFragment
 
 class FirstStartFragment : Fragment(R.layout.fragment_first_start) {
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<Button>(R.id.login_button).setOnClickListener {
+            navigateToAuthScreen()
+        }
+    }
+
+    private fun navigateToAuthScreen() {
+        val fragment = AuthFragment()
+        parentFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
+    }
 }
