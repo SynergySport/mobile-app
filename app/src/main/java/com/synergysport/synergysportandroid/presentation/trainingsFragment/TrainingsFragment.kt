7bd4e70f1b2766e0a6100c0dca91c40bf54f3dbb
Sergei <sk_developer@mail.ru>
2023-09-28 23:38:17 +0300

    Создан экран тренировок

---
+++ app/src/main/java/com/synergysport/synergysportandroid/presentation/trainingsFragment/TrainingsFragment.kt
@@ -0,0 +1,8 @@
+package com.synergysport.synergysportandroid.presentation.trainingsFragment
+
+import androidx.fragment.app.Fragment
+import com.synergysport.synergysportandroid.R
+
+class TrainingsFragment : Fragment(R.layout.fragment_trainings) {
+
+}
