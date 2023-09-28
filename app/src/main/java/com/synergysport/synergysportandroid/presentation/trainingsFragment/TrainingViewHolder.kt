fd519b606140a0cece5f6d3780928bc48fd711b0
Sergei <sk_developer@mail.ru>
2023-09-29 01:57:27 +0300

    Базовая верстка экрана тренировок

--- app/src/main/java/com/synergysport/synergysportandroid/presentation/trainingsFragment/TrainingViewHolder.kt
+++ app/src/main/java/com/synergysport/synergysportandroid/presentation/trainingsFragment/TrainingViewHolder.kt
@@ -1,11 +1,16 @@
 package com.synergysport.synergysportandroid.presentation.trainingsFragment
 
 import android.view.View
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.synergysport.synergysportandroid.R
 import com.synergysport.synergysportandroid.domain.entity.Training
 
 class TrainingViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
     fun bind(training: Training) {
-
+        view.findViewById<TextView>(R.id.training_type_tv).text = training.type
+        view.findViewById<TextView>(R.id.training_date_tv).text = training.date
+        view.findViewById<TextView>(R.id.cores_count_tv).text = training.cores.toString()
+        view.findViewById<TextView>(R.id.time_count_tv).text = training.time
     }
 }
--- app/src/main/java/com/synergysport/synergysportandroid/presentation/trainingsFragment/TrainingsFragment.kt
+++ app/src/main/java/com/synergysport/synergysportandroid/presentation/trainingsFragment/TrainingsFragment.kt
@@ -5,8 +5,12 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.synergysport.synergysportandroid.R
+import com.synergysport.synergysportandroid.domain.entity.Training
 
 class TrainingsFragment : Fragment(R.layout.fragment_trainings) {
+
+    private lateinit var trainingsListAdapter: TrainingsListAdapter
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initRecyclerView(view)
@@ -14,8 +18,16 @@ class TrainingsFragment : Fragment(R.layout.fragment_trainings) {
 
     private fun initRecyclerView(view: View) {
         val trainingsRv = view.findViewById<RecyclerView>(R.id.trainings_rv)
+        trainingsListAdapter = TrainingsListAdapter()
         with(trainingsRv) {
-            adapter = TrainingsListAdapter()
+            adapter = trainingsListAdapter
+            trainingsListAdapter.submitList(
+                listOf(
+                    Training(0, "Бег", "20.12.2020 17:00", 5, "16 min", "100km", "20km/h"),
+                    Training(1, "Ходьба", "20.12.2021 17:00", 2, "16 min", "100km", "20km/h"),
+                    Training(2, "Спортзал", "20.12.2022 17:00", 8, "16 min", "100km", "20km/h"),
+                )
+            )
         }
     }
 }
