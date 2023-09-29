b3c6a40c0cc3b05e1effe00651cbc45a3bddc486
Sergei <sk_developer@mail.ru>
2023-09-29 23:10:55 +0300

    Добавлен экран События

---
+++ app/src/main/java/com/synergysport/synergysportandroid/presentation/fragments/eventsFragment/EventsFragment.kt
@@ -0,0 +1,8 @@
+package com.synergysport.synergysportandroid.presentation.fragments.eventsFragment
+
+import androidx.fragment.app.Fragment
+import com.synergysport.synergysportandroid.R
+
+class EventsFragment : Fragment(R.layout.fragment_events) {
+
+}
