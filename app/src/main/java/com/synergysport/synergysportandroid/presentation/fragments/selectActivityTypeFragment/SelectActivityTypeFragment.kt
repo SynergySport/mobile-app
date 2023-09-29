d25f7cc9b669b0f57c9ec024c3d38a725b680f5f
Sergei <sk_developer@mail.ru>
2023-09-30 02:01:06 +0300

    создан SelectActivityTypeFragment

---
+++ app/src/main/java/com/synergysport/synergysportandroid/presentation/fragments/selectActivityTypeFragment/SelectActivityTypeFragment.kt
@@ -0,0 +1,7 @@
+package com.synergysport.synergysportandroid.presentation.fragments.selectActivityTypeFragment
+
+import androidx.fragment.app.Fragment
+import com.synergysport.synergysportandroid.R
+
+class SelectActivityTypeFragment : Fragment(R.layout.fragment_select_activity_type) {
+}
