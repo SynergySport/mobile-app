691a0b62b618ef76c6bb8c68a80ed90d603da767
Sergei <sk_developer@mail.ru>
2023-09-30 23:59:06 +0300

    Подключил viewbinding к TrackerFragment

--- app/src/main/java/com/synergysport/synergysportandroid/presentation/fragments/trackerFragment/TrackerFragment.kt
+++ app/src/main/java/com/synergysport/synergysportandroid/presentation/fragments/trackerFragment/TrackerFragment.kt
@@ -1,20 +1,32 @@
 package com.synergysport.synergysportandroid.presentation.fragments.trackerFragment
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
-import android.widget.ImageView
-import android.widget.TextView
+import android.view.ViewGroup
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import com.synergysport.synergysportandroid.R
 import com.synergysport.synergysportandroid.SynergySportApp
+import com.synergysport.synergysportandroid.databinding.FragmentTrackerBinding
 import com.synergysport.synergysportandroid.presentation.common.Navigator
 import javax.inject.Inject
 
 class TrackerFragment : Fragment(R.layout.fragment_tracker) {
 
+    private lateinit var binding: FragmentTrackerBinding
+
     @Inject
     lateinit var viewModel: TrackerFragmentViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentTrackerBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -30,13 +42,13 @@ class TrackerFragment : Fragment(R.layout.fragment_tracker) {
     }
 
     private fun initPauseButton() {
-        requireView().findViewById<ImageView>(R.id.pause_button).setOnClickListener {
+        binding.pauseButton.setOnClickListener {
             viewModel.onClickPauseResume()
         }
     }
 
     private fun initStopButton() {
-        with(requireView().findViewById<ImageView>(R.id.stop_button)) {
+        with(binding.stopButton) {
             setOnClickListener {
                 viewModel.onClickStop()
             }
@@ -47,14 +59,14 @@ class TrackerFragment : Fragment(R.layout.fragment_tracker) {
     private fun bindViewModel() {
         with(viewModel) {
             stepsCountLiveData.observe(viewLifecycleOwner) {
-                requireView().findViewById<TextView>(R.id.current_metric_value).text = it.toString()
+                binding.currentMetricValue.text = it.toString()
             }
             onPausedLiveData.observe(viewLifecycleOwner) {
-                requireView().findViewById<ImageView>(R.id.pause_button)
+                binding.pauseButton
                     .setImageResource(if (it) R.drawable.ic_play else R.drawable.ic_pause)
-                requireView().findViewById<ImageView>(R.id.stop_button).isVisible = it
+                binding.stopButton.isVisible = it
             }
-            closeScreenLiveData.observe(viewLifecycleOwner){
+            closeScreenLiveData.observe(viewLifecycleOwner) {
                 Navigator.closeFragment(parentFragmentManager)
             }
         }
